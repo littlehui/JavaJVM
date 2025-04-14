559be7acab33ac6cdb6f39000eb3413d8f2b2002
littlehui <812699887@qq.com>
2025-04-15 00:06:09 +0800

    解释器

--- chapter09/src/main/java/com/lilhui/jvm/interpreter/Interpreter.java
+++ chapter09/src/main/java/com/lilhui/jvm/interpreter/Interpreter.java
@@ -44,19 +44,27 @@ public class Interpreter {
     }
 
     private static void run(Thread thread, byte[] code, boolean logInst) {
+        //code是方法的指令集
         CodeReader reader = new CodeReader(code);
         while (true) {
+            //先获取当前帧
             Frame frame = thread.currentFrame();
+            //获取下一个PC计数器并缓存
             int pc = frame.getNextPC();
             thread.setPc(pc);
             reader.reset(frame.getMethod().getCode(), pc);
+            //读取一个字节，操作编码
             int opcode = reader.readU1();
+            //通过操作编码获取指令的实现
             Instruction instruction = Instructions.newInstruction(opcode);
+            //初始化指令相关的操作数
             instruction.readOperands(reader);
+            //设置下一帧的地址
             frame.setNextPC(reader.getPC());
             if (logInst) {
                 logInstruction(frame, instruction);
             }
+            //执行指令
             instruction.execute(frame);
             if (thread.isStackEmpty()) {
                 break;
